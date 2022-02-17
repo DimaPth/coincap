8903d7b7d7fe31d396abda8806ee711e6c50fffe
Дмитрий Пётух <mr.petuhdima@mail.ru>
2022-02-17 20:01:36 +0300

    fix formatNumber service

--- src/services/formatNumber.js
+++ src/services/formatNumber.js
@@ -1,9 +1,17 @@
 export const formatNumber = (num, price) => {
   if(price) {
+    if(num < 2) {
+      return new Intl.NumberFormat("en-US", {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 5
+      }).format(num);
+    }
     return new Intl.NumberFormat("en-US", {
       style: 'currency',
       currency: 'USD',
     }).format(num);
+
   }
   return new Intl.NumberFormat("en-US", {
     notation: "compact",
